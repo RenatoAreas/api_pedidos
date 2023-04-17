e9a3fef9def2050921daffc7f8081d10fe6b4869
Renato Areas <rsareas93@gmail.com>
2023-04-17 20:40:09 -0300

    desenvolvendo endpoint e logs

---
+++ src/main/java/br/com/cotiinformatica/dtos/PedidoResponseDto.java
@@ -0,0 +1,12 @@
+package br.com.cotiinformatica.dtos;
+
+import lombok.Data;
+
+@Data
+public class PedidoResponseDto {
+	
+	private String status;
+	private String numeroPedido;
+	private String mensagem;
+
+}
